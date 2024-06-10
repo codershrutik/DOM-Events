86828df2eba0ae5a831591392c2964cb88364d6f
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-10 03:59:55 +0000

    AddEventListener added

--- src/app.js
+++ src/app.js
@@ -13,3 +13,23 @@ btn.onmouseenter = scream;
 const hey = document.querySelector('h1').onclick = function(){
     alert('HIIIIIII!!!');
 }
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click',function(){
+    alert('CLICKED!!');
+})
+
+function twist(){
+    console.log("TWIST!")
+}
+
+function shout(){
+    console.log("SHOUT!")
+}
+
+const tasBtn = document.querySelector('#tas')
+    tasBtn.onclick = twist;
+    tasBtn.onclick = shout;
+
+    tasBtn.addEventListener('click',twist);
+    tasBtn.addEventListener('click', shout);
