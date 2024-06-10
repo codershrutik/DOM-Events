543f16a5f05dc4bdb7363e13703e40ab0dbe96fb
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-10 05:11:38 +0000

    Form event.prevent practice

--- form/app.js
+++ form/app.js
@@ -1,5 +1,12 @@
 const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName')
+const list = document.querySelector('#cats')
 form.addEventListener('submit',function(e){
     e.preventDefault();
     console.log('Submitted!');
+    const newLI = document.createElement('LIs');
+    newLI.innerText = catName;
+    console.log(newLI);
+    list.append(newLI);
+    input.value = "";
 })
