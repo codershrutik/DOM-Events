36684a5082e938b33f65ccfa73ceffcd21a9567b
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-10 05:43:19 +0000

    Event bubbling more

--- input/app.js
+++ input/app.js
@@ -1,7 +1,25 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+// const input = document.querySelector('input');
+// const h1 = document.querySelector('h1');
 
-input.addEventListener('input',function(){
-    h1.innerText = input.value();
-    console.log('INPUT EVENT!');
+// input.addEventListener('input',function(){
+//     h1.innerText = input.value();
+//     console.log('INPUT EVENT!');
+// })
+
+const button = document.querySelector('button');
+const container = document.querySelector('#container');
+
+button.addEventListener('click',function(){
+    container.computedStyleMap.backgorundColor = makeRandomColor;()
 })
+
+container.addEventListener('click',function(){
+    container.classList.toggle('hide');
+})
+
+const makeRandomColor = () => {
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+    return `rgb(${r},${g},${b})`;
+}
